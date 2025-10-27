e05a21e0f2acf4c7bdef4c24d62ba53506756f70
teamplayerguy <teamplayerguy@gmail.com>
2025-10-26 20:49:03 -0400

    forgot module.exports = router within oders.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -16,11 +16,15 @@ router.post('/', (req, res, next) => {
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order details',
+        orderId: req.params.orderId
     });
 });
 
 router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted',
+        orderId: req.params.orderId
     });
-}); 
+});
+
+module.exports = router;
